adce555006e8e2e77cf97870a059d5d747b3d814
ElizavetaAveryanova <153016425+ElizavetaAveryanova@users.noreply.github.com>
2024-07-05 15:44:00 +0500

    init_model_habits

--- habits/models.py
+++ habits/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 
-from users.models import User
-
 NULLABLE = {"blank": True, "null": True}
 
 
 class Reward(models.Model):
     """Вознаграждение действием"""
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор")
+    created_by = models.ForeignKey('users.User', on_delete=models.CASCADE, **NULLABLE, verbose_name='Автор')
     title = models.CharField(max_length=100, verbose_name="Название вознаграждения")
 
     def __str__(self):
@@ -18,19 +16,37 @@ class Reward(models.Model):
         verbose_name_plural = "Вознаграждения"
 
 
+class Place(models.Model):
+    """Место """
+    title = models.CharField(max_length=20, verbose_name='Название')
+    created_by = models.ForeignKey('users.User', on_delete=models.CASCADE, **NULLABLE, verbose_name='Автор')
+
+    def __str__(self):
+        return f'Место: {self.title}'
+
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
+
+
 class Habit(models.Model):
     """Привычка """
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Владелец привычки")
-    action = models.CharField(max_length=150, verbose_name="Действие")
-    place = models.CharField(max_length=100, verbose_name="Место выполнения привычки", **NULLABLE)
-    time = models.TimeField(verbose_name="Время выполнения привычки")
+    place = models.ForeignKey(Place, on_delete=models.SET_NULL, null=True, verbose_name='Место выполнения')
+    time = models.TimeField(verbose_name='Время выполнения')
+    action = models.CharField(max_length=250, verbose_name='Действие')
     period = models.PositiveSmallIntegerField(default=1, verbose_name='Периодичность выполнения в днях')
-    duration = models.DurationField(default=None, verbose_name="Продолжительность выполнения в секундах")
-    is_public = models.BooleanField(default=False, verbose_name="Признак публичности")
+    duration = models.PositiveSmallIntegerField(verbose_name='Продолжительность выполнения в секундах')
+    is_public = models.BooleanField(default=False, verbose_name='Признак публичности')
     is_pleasing_habit = models.BooleanField(default=False, verbose_name='Признак приятной привычки')
+    created_by = models.ForeignKey('users.User', on_delete=models.CASCADE, **NULLABLE, verbose_name='Автор')
+
     #  варианты вознаграждения
-    reward_with_action = models.ForeignKey(Reward, on_delete=models.SET_NULL, **NULLABLE, verbose_name='Вознаграждение действием')
-    reward_with_pleasing_habit = models.ForeignKey('self', on_delete=models.SET_NULL, **NULLABLE, verbose_name='Вознаграждение приятной привычкой')
+    reward_with_action = models.ForeignKey(Reward, on_delete=models.SET_NULL, null=True, blank=True,
+                                           verbose_name='Вознаграждение действием')
+    reward_with_pleasing_habit = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True,
+                                                   verbose_name='Вознаграждение приятной привычкой')
+
+
     def __str__(self):
         return f'Привычка: {self.action} в {self.time} в {self.place}'
 
