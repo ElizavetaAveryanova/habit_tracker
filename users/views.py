af3a958ef881e8b165f3dda546f8cce8cb0abab4
ElizavetaAveryanova <153016425+ElizavetaAveryanova@users.noreply.github.com>
2024-07-08 21:27:24 +0500

    course_work_

--- users/views.py
+++ users/views.py
@@ -13,6 +13,21 @@ class RegisterAPIView(generics.CreateAPIView):
     permission_classes = [AllowAny]
     serializer_class = RegisterSerializer
 
+    def create(self, validated_data):
+        user = User(
+            email=validated_data["email"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
+
+    # def perform_create(self, serializer):
+    #     user = serializer.save()
+    #     user.set_password(serializer.validated_data['password'])
+    #     user.save()
+
 
 class MyTokenObtainPairView(TokenObtainPairView):
     """Контроллер для получения JWT-токена"""
