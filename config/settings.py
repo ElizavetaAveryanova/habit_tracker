eede40a6f3b4fcc060316c645187793c6ec4cb80
ElizavetaAveryanova <153016425+ElizavetaAveryanova@users.noreply.github.com>
2024-07-24 19:01:17 +0500

    course_work_setting

--- config/settings.py
+++ config/settings.py
@@ -96,10 +96,15 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = (
-    BASE_DIR / 'static'
-)
-STATIC_ROOT = BASE_DIR / 'static'
+
+ENV_TYPE = os.getenv("ENV_TYPE")
+
+if ENV_TYPE == 'local':
+    STATICFILES_DIRS = (
+        BASE_DIR / 'static'
+    )
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
