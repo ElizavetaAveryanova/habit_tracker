b979621e18df3d661946efbc9db38e0f2c26a2a7
ElizavetaAveryanova <153016425+ElizavetaAveryanova@users.noreply.github.com>
2024-07-29 14:13:00 +0500

    course_work_gitlab

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 
 from django.core.management import BaseCommand
@@ -17,23 +16,4 @@ class Command(BaseCommand):
         )
         user.set_password(os.getenv("ADMIN_PASSWORD"))
         user.save()
-=======
-import os
 
-from django.core.management import BaseCommand
-from users.models import User
-
-
-class Command(BaseCommand):
-    """Создание superuser"""
-
-    def handle(self, *args, **options):
-        user = User.objects.create(
-            email="admin@habits.ru",
-            is_staff=True,
-            is_superuser=True,
-            is_active=True,
-        )
-        user.set_password(os.getenv("ADMIN_PASSWORD"))
-        user.save()
->>>>>>> b06ae89223852ac8726da1a522d7e09db2c8c7e7
